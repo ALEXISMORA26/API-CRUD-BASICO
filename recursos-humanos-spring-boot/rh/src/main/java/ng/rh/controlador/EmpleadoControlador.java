da477b5af4edf6e0221f5e1503c1b667b069352f
ALEXISMORA26 <moraaguilaralexis26@gmail.com>
2023-09-02 14:57:41 -0600

    agregar, buscar por id front y back

--- recursos-humanos-spring-boot/rh/src/main/java/ng/rh/controlador/EmpleadoControlador.java
+++ recursos-humanos-spring-boot/rh/src/main/java/ng/rh/controlador/EmpleadoControlador.java
@@ -1,15 +1,14 @@
 package ng.rh.controlador;
 
 
+import ng.rh.excepcion.RecursoNoEncontradoExcepcion;
 import ng.rh.modelo.Empleado;
 import ng.rh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +27,22 @@ public class EmpleadoControlador {
         var empleados = empleadoServicio.listarEmpleados();
         empleados.forEach(empleado -> LOGGER.info(empleado.toString()));
         return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        LOGGER.info("Empleado a agreagr: " + empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
+
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if(empleado == null){
+            throw new RecursoNoEncontradoExcepcion("no se encontro el id: " + id);
+        }
+        return ResponseEntity.ok(empleado);
     }
 
 
